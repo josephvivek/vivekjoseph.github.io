2dfa8521f39287c954ad405423e909010177f4ae
josephvivek <112967087+josephvivek@users.noreply.github.com>
2022-09-06 22:29:02 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,13 +3,20 @@ function check(){
         alert("Enter head count!!");
     }
     else {
-        if(document.getElementById("entry").value == 0 || !(Number.isInteger(Number(document.getElementById("entry").value)))){
+        if(document.getElementById("entry").value <0){
             alert("Enter valid head count!!");
         }
         else{
-            window.location.href = "page2.html";
-            localStorage.setItem("mvalue",document.getElementById("entry").value);
+            if(document.getElementById("entry").value == 0 || !(Number.isInteger(Number(document.getElementById("entry").value)))){
+                alert("Enter valid head count!!");
+            }
+            else{
+                window.location.href = "page2.html";
+                localStorage.setItem("mvalue",document.getElementById("entry").value);
+            }
+
         }
+        
     }
     // localStorage.setItem("mvalue",document.getElementById("entry").value);
     
